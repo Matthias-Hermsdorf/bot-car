cb8779dd117d0f8edd3bcfc768d663e714a436bb
Matthias Hermsdorf <matthiashermsdorf@gmx.de>
2017-03-12 09:51:45 +0100

    Änderung von bot-add zu device-add type="bot"

--- connect-market.js
+++ connect-market.js
@@ -1,6 +1,7 @@
 var market = require('socket.io-client')('http://localhost:3095');
+let ip = require('my-local-ip')();
     
 market.on('connect', function(){ 
     console.log("connecect to :3095") 
-    market.emit("bot-add",{name:"C3PO"})
+    market.emit("device-add",{name:"C3PO",host:ip+":4002", type:"car"})
 });
